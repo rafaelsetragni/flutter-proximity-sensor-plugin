154b844529dc15a52ce3459adb9864381b8d4e69
Rafael Setragni <rafaelsetra@gmail.com>
2022-06-25 18:33:59 -0300

    Added automatic wakelock for proximity sensor

--- android/src/main/kotlin/dev/jeremyko/proximity_sensor/ProximitySensorPlugin.kt
+++ android/src/main/kotlin/dev/jeremyko/proximity_sensor/ProximitySensorPlugin.kt
@@ -3,22 +3,38 @@ package dev.jeremyko.proximity_sensor
 
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.EventChannel
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-class ProximitySensorPlugin: FlutterPlugin  {
+class ProximitySensorPlugin: FlutterPlugin, ActivityAware {
   private lateinit var channel : EventChannel
   private lateinit var streamHandler : ProximityStreamHandler
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = EventChannel(flutterPluginBinding.binaryMessenger, "proximity_sensor")
     streamHandler = ProximityStreamHandler( flutterPluginBinding.applicationContext,
-                                            flutterPluginBinding.binaryMessenger)
+      flutterPluginBinding.binaryMessenger)
     channel.setStreamHandler(streamHandler)
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setStreamHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    streamHandler.activity = binding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    streamHandler.activity = null
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
   }
 }
 
--- android/src/main/kotlin/dev/jeremyko/proximity_sensor/ProximityStreamHandler.kt
+++ android/src/main/kotlin/dev/jeremyko/proximity_sensor/ProximityStreamHandler.kt
@@ -1,10 +1,17 @@
 package dev.jeremyko.proximity_sensor
 
+import android.app.Activity
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
+import android.os.PowerManager
+import android.os.PowerManager.WakeLock
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import io.flutter.Log
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.EventChannel
 import java.io.IOException
@@ -12,9 +19,15 @@ import java.lang.UnsupportedOperationException
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 class ProximityStreamHandler(
-        private val applicationContext: Context,
-        private val messenger: BinaryMessenger
+    private val applicationContext: Context,
+    private val messenger: BinaryMessenger
 ): EventChannel.StreamHandler, SensorEventListener {
+
+    var activity: Activity? = null
+    private val powerManager: PowerManager =
+        applicationContext.getSystemService(Context.POWER_SERVICE)!! as PowerManager
+
+    private var wakeLock:WakeLock? = null
 
     private var eventSink: EventChannel.EventSink? = null
     private lateinit var sensorManager: SensorManager
@@ -24,29 +37,44 @@ class ProximityStreamHandler(
         eventSink = events
         sensorManager =  applicationContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) ?:
-            throw UnsupportedOperationException("proximity sensor unavailable")
+                throw UnsupportedOperationException("proximity sensor unavailable")
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if(wakeLock == null)
+                wakeLock = powerManager.newWakeLock(
+                    PowerManager.PROXIMITY_SCREEN_OFF_WAKE_LOCK,
+                    "lock:proximity_screen_off")
+
+            if(!wakeLock!!.isHeld){
+                wakeLock!!.acquire(10 * 60 * 1000L /*10 minutes*/)
+                Log.d("ProdimityStreamHandler", "WakeLock("+wakeLock.hashCode()+") acquired")
+            }
+        }
+
+        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onCancel(arguments: Any?) {
         sensorManager.unregisterListener(this, proximitySensor)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if(wakeLock != null && wakeLock!!.isHeld){
+                Log.d("ProdimityStreamHandler", "WakeLock("+wakeLock.hashCode()+") released")
+                wakeLock!!.release()
+                wakeLock = null
+            }
+        }
+
+        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        val distance = event ?.values?.get(0)?.toInt() //get distance
-        if (distance != null) {
-            if(distance > 0) {
-                // distance > 0 , nothing is near
-                eventSink ?.success(0)
-            }else{
-                // distance == 0, something is near
-                eventSink ?.success(1)
-            }
-        }
+        val distance = event ?.values?.get(0)?.toInt() ?: return //get distance
+        eventSink?.success(if (distance > 0) 0 else 1)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        //do nothing
+
     }
 }
